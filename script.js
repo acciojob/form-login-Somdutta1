14b591abedb0a6fe83c4f87932be4204339d4e93
Somdutta1 <127023278+Somdutta1@users.noreply.github.com>
2024-02-18 20:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+ let inp = document.querySelecetorAll("input")
+ let first_name = inp[0].value
+ let last_name = inp[1].value
+ let fullname = first_name+" "+last_name
+ alert(fullname)	
 }
